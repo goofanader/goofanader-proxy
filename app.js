66a27e22651701c9417180a4f1bc4150975b3925
Phyllis Douglas <goofanader@gmail.com>
2025-05-02 18:00:05 -0700

    add cache-control

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ function enableCORS(req, res, next) {
   res.header('Access-Control-Allow-Credentials', true)
   res.header(
     'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Content-Encoding, Accept'
+    'Origin, X-Requested-With, Content-Type, Content-Encoding, Accept, Cache-Control'
   )
   res.header(
     'Access-Control-Allow-Methods',
